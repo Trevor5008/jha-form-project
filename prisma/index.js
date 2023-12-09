6809529214159d1f4023fcd196dd5bc02eec95ea
Trevor Werner <trezrevner@icloud.com>
2023-12-09 07:49:51 -0500

    Added data options for each category, still working on ui functionality

--- prisma/index.js
+++ prisma/index.js
@@ -1,17 +1,101 @@
 const { PrismaClient } = require("@prisma/client")
-const {permits} = require('../lib/options')
+const { 
+    categories,
+    permits,
+    atmosphericMonitoring,
+    situations,
+    hazards,
+    hazardControls,
+    properPpe,
+    emergencyResponse
+    } = require("../lib/options")
 
 const prisma = new PrismaClient()
 
+const options = ["permits"]
+
 async function main() {
+    // Populate categories
+    let categoriesArr = []
+    for (let category of categories) {
+        const cat = await prisma.category.create({
+            data: {
+                name: category
+            }
+        })
+        categoriesArr.push(cat)
+    }
+
     for (let permit of permits) {
-        await prisma.CategoryOption.createMany({
+        await prisma.categoryOption.create({
             data: {
                 name: permit,
-                categoryId: 1
+                categoryId: categoriesArr[0].id
             }
         })
     }
+    categoriesArr.shift()
+
+    for (let atmosphericCondition of atmosphericMonitoring) {
+        await prisma.categoryOption.create({
+            data: {
+                name: atmosphericCondition,
+                categoryId: categoriesArr[0].id
+            }
+        })
+    }
+
+    categoriesArr.shift()
+
+    for (let situation of situations) {
+        await prisma.categoryOption.create({
+            data: {
+                name: situation,
+                categoryId: categoriesArr[0].id
+            }
+        })
+    }
+    categoriesArr.shift()
+
+    for (let hazard of hazards) {
+        await prisma.categoryOption.create({
+            data: {
+                name: hazard,
+                categoryId: categoriesArr[0].id
+            }
+        })
+    }
+    categoriesArr.shift()
+
+    for (let hazardControl of hazardControls) {
+        await prisma.categoryOption.create({
+            data: {
+                name: hazardControl,
+                categoryId: categoriesArr[0].id
+            }
+        })
+    }
+    categoriesArr.shift()
+
+    for (let ppe of properPpe) {
+        await prisma.categoryOption.create({
+            data: {
+                name: ppe,
+                categoryId: categoriesArr[0].id
+            }
+        })
+    }
+    categoriesArr.shift()
+
+    for (let response of emergencyResponse) {
+        await prisma.categoryOption.create({
+            data: {
+                name: response,
+                categoryId: categoriesArr[0].id
+            }
+        })
+    }
+    categoriesArr.shift()
 }
 
 main()
