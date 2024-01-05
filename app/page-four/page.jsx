bc360121b44939d0b5e2dd4cb14834f44c026319
Trevor Werner <trezrevner@icloud.com>
2024-01-05 15:47:22 -0500

    Now rendering hazard options with checkbox functionality

--- app/page-four/page.jsx
+++ app/page-four/page.jsx
@@ -20,21 +20,224 @@ export default function PageFour() {
    const searchParams = useSearchParams()
    const shiftId = searchParams.get("id")
    const [hazards, setHazards] = useState(null)
-   const [dataReady, setDataReady] = useState(false)
+   const [dataReady, setDataReady] =
+      useState(false)
 
    useEffect(() => {
-    fetch(
-       "../api/get-page4-options/" +
-          shiftId
-    )
-    //    .then((res) => res.json())
-    //    .then((res) => {
-    //       setSituations(res.situationOpts)
-    //       setSituationsMisc(
-    //          res.situationsMisc?.details
-    //       )
-    //    })
-    //    .then(() => setDataReady(true))
- }, [])
-   return <div>{shiftId}</div>
+      fetch("../api/get-page4-options/" + shiftId)
+         .then((res) => res.json())
+         .then((res) => {
+            setHazards(res.hazardOpts)
+            //   setSituationsMisc(
+            //      res.situationsMisc?.details
+            //   )
+         })
+         .then(() => setDataReady(true))
+   }, [])
+
+   function handleHazardChange(
+    idx,
+    isChecked
+ ) {
+    const val = isChecked === "true"
+    setHazards((prev) => {
+       const updatedOpts = [...prev]
+       updatedOpts[
+          idx
+       ].shiftCategoryOptions[0].checked = val
+       return updatedOpts
+    })
+ }
+   return (
+      <Container>
+         {/* Hazards */}
+         <Container>
+            {/* Heading */}
+            <Typography
+               variant="h3"
+               marginBottom={1}
+            >
+               <span
+                  style={{ fontWeight: "bold" }}
+               >
+                  C.2
+               </span>{" "}
+               Hazards
+            </Typography>
+            {/* Body */}
+            <Box
+               sx={{
+                  display: "flex",
+                  flexDirection: "column",
+                  paddingX: {
+                     sm: 1
+                  }
+               }}
+            >
+               {dataReady
+                  ? hazards.map((opt, idx) => {
+                       return (
+                          <Box display="flex">
+                             <FormControl
+                                className="mb-2 flex flex-nowrap items-center justify-between"
+                                fullWidth
+                                sx={{
+                                   flexDirection:
+                                      "row"
+                                }}
+                             >
+                                <FormLabel
+                                   id={`${opt.name} radio group`}
+                                   className="text-black"
+                                   sx={{
+                                      fontSize: {
+                                         xs: 14,
+                                         sm: 16
+                                      },
+                                      width: "50%"
+                                   }}
+                                >
+                                   {opt.name}
+                                </FormLabel>
+                                <RadioGroup
+                                   aria-labelledby={`${opt.name} radio group`}
+                                   name={`${opt.name}`}
+                                   onChange={(
+                                      e
+                                   ) =>
+                                      handleHazardChange(
+                                         idx,
+                                         e.target
+                                            .value
+                                      )
+                                   }
+                                   value={
+                                      opt
+                                         .shiftCategoryOptions[0]
+                                         .checked
+                                   }
+                                   sx={{
+                                      display:
+                                         "inline-block",
+                                      marginLeft: 1
+                                   }}
+                                >
+                                   <FormControlLabel
+                                      value={true}
+                                      control={
+                                         <Radio
+                                            checkedIcon={
+                                               <CheckIcon />
+                                            }
+                                            icon={
+                                               <CheckBoxOutlineBlankOutlinedIcon />
+                                            }
+                                         />
+                                      }
+                                      label="Yes"
+                                      sx={{
+                                         marginRight:
+                                            {
+                                               xs: 2
+                                            },
+                                         "& .MuiTypography-root":
+                                            {
+                                               fontSize:
+                                                  {
+                                                     xs: 14,
+                                                     sm: 16
+                                                  }
+                                            },
+                                         "& .MuiSvgIcon-root":
+                                            {
+                                               width: {
+                                                  xs: 20,
+                                                  sm: 24
+                                               }
+                                            }
+                                      }}
+                                   />
+                                   <FormControlLabel
+                                      value={
+                                         false
+                                      }
+                                      control={
+                                         <Radio
+                                            checkedIcon={
+                                               <CheckIcon />
+                                            }
+                                            icon={
+                                               <CheckBoxOutlineBlankOutlinedIcon />
+                                            }
+                                         />
+                                      }
+                                      label="No"
+                                      sx={{
+                                         marginRight:
+                                            {
+                                               xs: 0,
+                                               sm: 1
+                                            },
+                                         "& .MuiTypography-root":
+                                            {
+                                               fontSize:
+                                                  {
+                                                     xs: 14,
+                                                     sm: 16
+                                                  }
+                                            },
+                                         "& .MuiButtonBase-root":
+                                            {
+                                               width: {
+                                                  xs: 34,
+                                                  sm: 40
+                                               }
+                                            },
+                                         "& .MuiSvgIcon-root":
+                                            {
+                                               width: {
+                                                  xs: 20,
+                                                  sm: 24
+                                               }
+                                            }
+                                      }}
+                                   />
+                                </RadioGroup>
+                             </FormControl>
+                          </Box>
+                       )
+                    })
+                  : null}
+            </Box>
+         </Container>
+         {/* Navigation Buttons */}
+         <Box
+            display="flex"
+            justifyContent="space-evenly"
+         >
+            <Button variant="standard">
+               <Link
+                  href={{
+                     pathname: "../page-three",
+                     query: { id: shiftId }
+                  }}
+                  //   onClick={handleNext}
+               >
+                  Previous
+               </Link>
+            </Button>
+            <Button variant="standard">
+               <Link
+                  href={{
+                     pathname: "../page-four",
+                     query: { id: shiftId }
+                  }}
+                  //   onClick={handleNext}
+               >
+                  Next
+               </Link>
+            </Button>
+         </Box>
+      </Container>
+   )
 }
