19c8a7f7330d950d1a9e17a2225bf109d0a220e2
Trevor Werner <trezrevner@icloud.com>
2024-02-08 21:39:28 -0500

    Started template ui functionality on home page, needs organization

--- app/components/HomePage.jsx
+++ app/components/HomePage.jsx
@@ -1,5 +1,5 @@
-"use client"
-import { useEffect, useState } from "react"
+"use client";
+import { useEffect, useState } from "react";
 import {
    Typography,
    Container,
@@ -8,95 +8,106 @@ import {
    Box,
    InputLabel,
    Select,
-   MenuItem
-} from "@mui/material"
-import Link from "next/link"
-import { FormControl } from "@mui/material"
-import DateTimeInput from "./DateTimeInput"
-import SelectInput from "./SelectInput"
-import {
-   projectData,
-   companyNames,
-   supervisors,
-   foreman
-} from "@/lib/options"
+   MenuItem,
+   ListItemText,
+   ListItemButton,
+   List,
+   ListSubheader,
+} from "@mui/material";
+import Link from "next/link";
+import { FormControl } from "@mui/material";
+import DateTimeInput from "./DateTimeInput";
+import SelectInput from "./SelectInput";
+import { projectData, companyNames, supervisors, foreman } from "@/lib/options";
 
 export default function HomePage() {
-   const [projects, setProjects] = useState(null)
-   const [projectName, setProjectName] =
-      useState("")
+   const [projects, setProjects] = useState(null);
 
-   const [shift, setShift] = useState(null)
-   const [dataReady, setDataReady] =
-      useState(false)
+   const [shift, setShift] = useState(null);
+   const [dataReady, setDataReady] = useState(false);
 
    useEffect(() => {
-      loadProjects()
-      console.log("loaded")
-   }, [])
+      loadProjects();
+   }, []);
 
    async function loadProjects() {
       fetch("../../api/load-projects")
          .then((res) => res.json())
          .then((res) => setProjects(res.projects))
-         .then(() => setDataReady(true))
+         .then(() => setDataReady(true));
    }
 
    return dataReady ? (
       <Container>
-         <h1>Tasks</h1>
-         {projects.map((opt, idx) => {
-            return (
-               <Container>
-                  <ul>
-                     {opt.name}
-                     {opt.Shifts.map((shift) => {
-                        return (
-                           <li>
-                              <Link
-                                 style={{
-                                    textDecoration:
-                                       "none"
-                                 }}
-                                 sx={{
-                                    cursor:
-                                       "pointer"
-                                 }}
-                                 href={{
-                                    pathname:
-                                       "../page-two",
-                                    query: {
-                                       id: shift.id
-                                    }
-                                 }}
-                              >
-                                 {
-                                    shift.description
-                                 }
-                              </Link>
-                           </li>
-                        )
-                     })}
-                  </ul>
-                  <Button>
-                     <Link
-                        href={{
-                           pathname: "../page-one",
-                           query: {
-                              id: opt.id
-                           }
-                        }}
-                        style={{
-                           textDecoration: "none",
-                           cursor: "pointer"
-                        }}
-                     >
-                        New Shift
-                     </Link>
-                  </Button>
-               </Container>
-            )
-         })}
+         <Typography variant="h2">Project(s)</Typography>
+         <List>
+            {projects.map((opt, idx) => {
+               return (
+                  <Box key={idx} marginLeft={2}>
+                     <ListItemText>{opt.name}</ListItemText>
+                     <Box marginLeft={2}>
+                        <Typography variant="h3">Task(s)</Typography>
+                        {opt.Shifts.map((shift, idx) => {
+                           return (
+                              <Box key={idx} marginLeft={2}>
+                                 <Button variant="outlined">
+                                    <Link
+                                       style={{
+                                          textDecoration: "none",
+                                          fontSize: 12
+                                       }}
+                                       sx={{
+                                          cursor: "pointer",
+                                       }}
+                                       href={{
+                                          pathname: "../page-two",
+                                          query: {
+                                             id: shift.id,
+                                          },
+                                       }}
+                                    >
+                                       New Shift
+                                    </Link>
+                                 </Button>
+                                 {/* Template has no shift id */}
+                                 <Button variant="outlined">
+                                    <Link
+                                       style={{
+                                          textDecoration: "none",
+                                          fontSize: 12
+                                       }}
+                                       sx={{
+                                          cursor: "pointer",
+                                       }}
+                                       href={{
+                                          pathname: "../page-two",
+                                       }}
+                                    >
+                                       Create Template
+                                    </Link>
+                                 </Button>
+                              </Box>
+                           );
+                        })}
+                     </Box>
+                     <Button variant="outlined" sx={{ marginLeft: 2 }}>
+                        {/* NextJs link, not Material UI */}
+                        <Link
+                           href={{
+                              pathname: "../page-one",
+                              query: {
+                                 id: opt.id,
+                              },
+                           }}
+                           style={{ textDecoration: "none" }}
+                        >
+                           New Task
+                        </Link>
+                     </Button>
+                  </Box>
+               );
+            })}
+         </List>
       </Container>
-   ) : null
+   ) : null;
 }
